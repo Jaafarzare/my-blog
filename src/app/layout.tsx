1a0f75bd2703a5103952ea4e1899086bf83dde98
Jaafarzare <146831768+Jaafarzare@users.noreply.github.com>
2025-02-23 23:03:04 +0330

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
-import Sidebar from "@/components/Sidebar";
 import Header from "@/components/Header";
 import localFont from "next/font/local";
 
